a8c5b49045d47b81ed16c3b07ab30248e47f1b53
Gabriel Heinrich <gabriel@devhausleipzig.de>
2021-03-25 10:46:50 +0000

    Extract loud function

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,33 @@ const sayCommandLineArguments = function () {
   console.log(process.argv)
 }
 
+const loud = function(text: string) {
+  // body
+  const loudText = text.toUpperCase() + "!";
+  return loudText;
+};
+
 const say = function(message: string) {
   console.log(message)
 }
 
 const shout = function(message: string) {
-  const messageUpperCasedAndWithBang = message.toUpperCase() + "!";
-  console.log(messageUpperCasedAndWithBang)
+  const loudMessage = loud(message);
+  console.log(loudMessage)
 }
 
-const myMessage: string = "Hello"
+
 
 // console.log("Hello Typescript")
 // sayHelloTypescript();
-say("Hello Typescript");
+// say("Hello Typescript");
 shout("Hello Typescript");
+
+// console.log("BELEN!", "Hello Belen")
+// console.log(loud("Belen"), "Hello Belen");
+
+// const myBox = new Date();
+// const myBox = Math.random();
 
 // const myHelloMessage = "Hello to all of you"
 // console.log(myHelloMessage.toUpperCase() + "!")
@@ -30,6 +42,6 @@ shout("Hello Typescript");
 // Logs the command line arguments
 // sayCommandLineArguments();
 // .join(): convert an array into a string
-say(process.argv.join(",\n"));
-shout(process.argv.join(",\n"));
+// say(process.argv.join(",\n"));
+// shout(process.argv.join(",\n"));
 
