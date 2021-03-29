7f8dcc54bbcb394659efaf232290f0699165c2ec
Gabriel Heinrich <gabriel@devhausleipzig.de>
2021-03-29 13:41:56 +0000

    Read and parse recipe from recipe.json file

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,18 @@
 import {h1, h2, list} from "./markdown"
+import {readFileSync} from "fs"
 
 interface Recipe {
   title: string,
   ingredients: string[]
 }
 
-const recipes: Recipe[] = [{
-  title : "Best Omlette Ever",
-  ingredients: ["6 eggs", "1/4 tsp salt", "2 tbsp butter"]
-}, {
-  title : "Best Lasagne",
-  ingredients: ["Tomatoes", "You know the rest"]
-}]
-
 const recipeToMarkdown = function(recipe: Recipe): string {
   return h1(recipe.title) + h2("Ingredients") + list(recipe.ingredients);
 }
 
-const recipe0AsMarkdown = recipeToMarkdown(recipes[0])
-console.log(recipe0AsMarkdown);
+const jsonFile = readFileSync("./recipe.json", {encoding: 'utf-8'});
+
+const parsedJson = JSON.parse(jsonFile);
+
+const myRecipeAsMarkdown = recipeToMarkdown(parsedJson);
+console.log(myRecipeAsMarkdown)
